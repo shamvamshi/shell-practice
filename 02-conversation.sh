5061885a6d3181dd6549357a76867b4c9f3005a3
shamvamshi <shamvamshiwork@gmail.com>
2025-06-03 18:47:17 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey Musk, How are you?"
+echo "Musk:: Iam good Trump, How are you doing"
+echo "Trump: iam fine too. Whats up?"
+echo "Musk:: Nothing, just going to Sun now, will you come?"
+echo "Trump: Sorry, you carry on! I will come once you come back"
