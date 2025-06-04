25b636a5959e286a457402d4ad2f146b5929f0e2
shamvamshi <shamvamshiwork@gmail.com>
2025-06-04 17:22:05 +0530

    Hello world started

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,13 +1,52 @@
 #!/bin.bash
 
+    # NUMBER=$1
+
+    # if [ $NUMBER -lt 10 ]
+    # then
+
+    # echo "given number $NUMBER is less than 10"
+    
+    # else 
+    #     echo "given number $NUMBER is greater than 10"
+    
+    # fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     NUMBER=$1
 
-    if [ $NUMBER -lt 10 ]
-    then
+    if [ $NUMBER1 is -gt 10 ]
 
-    echo "given number $NUMBER is less than 10"
-    
-    else 
-        echo "given number $NUMBER is greater than 10"
-    
+    then
+    echo "given number is $NUMBER greater than 10"
+    else
+         echo "given number is $NUMBER is less than 10"
+
     fi
+
