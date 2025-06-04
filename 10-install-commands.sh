e9e298b62ed3742db902818855d64061a079358d
shamvamshi <shamvamshiwork@gmail.com>
2025-06-04 19:47:01 +0530

    Hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 if [$USERID -ne 0]
 then
     echo "Error : please run this script with root access"
+    exit 1 # exit other than 0 to 127
 else 
     echo "you are running this with root access"
 fi
