80f6a4df3be87a6ef653f75b5f1e5b97c13bcf52
shamvamshi <shamvamshiwork@gmail.com>
2025-06-04 14:08:36 +0530

    Hello world started

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-NUMBER1=500
-NUMBER2=3
+# NUMBER1=500
+# NUMBER2=3
 
-SUM=$(($NUMBER1*$NUMBER2))
+# SUM=$(($NUMBER1*$NUMBER2))
 
-echo "SUM OF $NUMBER1 AND $NUMBER2 is: $SUM"
+# echo "SUM OF $NUMBER1 AND $NUMBER2 is: $SUM"
+
+NUMBER1=200
+NUMBER2=200
+ TIMESTAMP=$(DATE)
+ echo "this script is executed at: $TIMESTAMP"
