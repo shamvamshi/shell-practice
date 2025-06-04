9676ea3ac7cdff3ab308361b40f0d8cf746648e0
shamvamshi <shamvamshiwork@gmail.com>
2025-06-04 16:23:43 +0530

    Hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -5,7 +5,7 @@ echo "number of variables: $#"
 echo "script name: $0"
 echo "current directory: $PWD"
 echo "user running the script: $USER"
-ECHO "home directory of user: $HOME"
+echo "home directory of user: $HOME"
 echo "PID of the script: $$"
 sleep 10 &
 echo "PID of the last command in background: $!"
