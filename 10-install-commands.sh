3c1b6fed4648255231e4a426bfeb547eba37b3d6
shamvamshi <shamvamshiwork@gmail.com>
2025-06-04 19:49:23 +0530

    Hello world started

--- 10-install-commands.sh
+++ 10-install-commands.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "Error : please run this script with root access"
     exit 1 # exit other than 0 to 127
 else 
     echo "you are running this with root access"
 fi
-
 dnf install mysql -y
