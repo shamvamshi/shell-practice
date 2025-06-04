6e3fd335d838a75a30e87795836c32b9072f0847
shamvamshi <shamvamshiwork@gmail.com>
2025-06-04 17:22:50 +0530

    Hello world started

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -44,9 +44,9 @@
     if [ $NUMBER1 is -gt 10 ]
 
     then
-    echo "given number is $NUMBER greater than 10"
+    echo "given number is $NUMBER less than 10"
     else
-         echo "given number is $NUMBER is less than 10"
+         echo "given number is $NUMBER is greater than 10"
 
     fi
 
